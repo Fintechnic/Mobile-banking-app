7279ee05a878dc24fbc11e32c7b837156f9664e4
Đỗ Như Quân <nhuquan.dev.py@gmail.com>
2025-03-31 16:51:10 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,16 @@ import 'dart:ui'; // 🛠 Import dart:ui để sử dụng PlatformDispatcher
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/auth_provider.dart';
-import 'screens/homepage.dart';
+import 'screens/homepage_screen.dart';
 import 'screens/login_screen.dart';
-import './pages/transferpage.dart';
-import './pages/qr_page.dart';     //Import QRpage
+import 'screens/transferpage.dart';
+import 'screens/qr_scan_screen.dart';     //Import QR screen
+import 'screens/Account_and_card.dart'; //Import Account_and_card screen
+import 'screens/pay_bill.dart'; //Import pay bill screen
+import 'screens/profile_screen.dart'; // Import Profile screen
+import 'screens/sign_up_screen.dart'; // Import sign_up screen
+import 'screens/bank_slip_screen.dart'; //Import bank slip screen
+import 'screens/my_qr_screen.dart'; // Import my qr screen
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -35,7 +41,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue),
       home: Consumer<AuthProvider>(
         builder: (context, authProvider, child) {
-          return authProvider.isAuthenticated ? const HomePage() : const LoginScreen();
+          return authProvider.isAuthenticated ? const HomePageScreen() : const LoginScreen();
         },
       ),
     );
